adb4a3ddae391f2b6047a497f9bc65462d40c9c3
easy1090 <congzhe@bytedance.com>
2025-03-31 16:57:10 +0800

    feat: circular

--- crates/rspack_plugin_circular_dependency/src/lib.rs
+++ crates/rspack_plugin_circular_dependency/src/lib.rs
@@ -1,10 +1,17 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
 use rspack_core::{Compilation, CompilationOptimizeModules, CompilerOptions, Module, Plugin};
 use rspack_error::{Error, Result};
 use rspack_hook::{plugin, plugin_hook};
 use rspack_regex::RspackRegex;
 use rspack_util::path::relative;
+
+pub struct CircularDependencyDetectionContext<'a> {
+  pub module: &'a dyn Module,
+  pub paths: Vec<String>,
+  pub compilation: &'a Compilation,
+}
 
 // #[derive(Debug)]
 pub struct CircularDependencyPluginOptions {
@@ -13,7 +20,8 @@ pub struct CircularDependencyPluginOptions {
   pub include: Option<RspackRegex>,
   pub fail_on_error: bool,
   pub allow_async_cycles: bool,
-  pub on_detected: Option<Box<dyn Fn(&dyn Module, Vec<String>, &Compilation) -> Result<()>>>,
+  // pub on_detected:
+  //   Option<Arc<dyn Fn(&CircularDependencyDetectionContext) -> Result<()> + Send + Sync>>,
   pub cwd: String,
 }
 
@@ -30,7 +38,7 @@ impl CircularDependencyPlugin {
       include: RspackRegex::new(".*").ok(),
       fail_on_error: false,
       allow_async_cycles: false,
-      on_detected: None,
+      // on_detected: None,
       cwd: std::env::current_dir()
         .unwrap()
         .to_string_lossy()
@@ -50,7 +58,7 @@ impl CircularDependencyPlugin {
   ) -> Option<Vec<String>> {
     let cwd = self.options.cwd.to_string();
 
-    let dependencies = initial_module.dependencies;
+    // let dependencies = initial_module.dependencies;
 
     // Add the current module to the seen modules cache
     seen_modules.insert(current_module.debug_id, true);
@@ -63,52 +71,49 @@ impl CircularDependencyPlugin {
 
     // Iterate over the current modules dependencies
     for dependency in current_module.get_dependencies() {
-      if let Some(dep_module) = if let Some(module_graph) = &compilation.module_graph {
-        // handle getting a module for webpack 5
-        module_graph.get_module(dependency)
-      } else {
-        // handle getting a module for webpack 4
-        dependency
-      } {
-        // ignore dependencies that don't have an associated resource
-        if dep_module.resource.is_none() {
-          continue;
-        }
-        // ignore dependencies that are resolved asynchronously
-        if self.options.allow_async_cycles.unwrap_or(false) && dependency.weak {
-          continue;
-        }
-        // the dependency was resolved to the current module due to how webpack internals
-        // setup dependencies like CommonJsSelfReferenceDependency and ModuleDecoratorDependency
-        if current_module == dep_module {
-          continue;
-        }
+      let mut module_graph = compilation.get_module_graph();
+      let Some(dep_module) = module_graph.get_module_by_dependency_id(dependency) else {
+        continue;
+      };
 
-        if seen_modules.contains_key(&dep_module.debug_id) {
-          if dep_module.debug_id == initial_module.debug_id {
-            // Initial module has a circular dependency
-            let current_resource = current_module.resource.as_ref().unwrap();
-            let dep_resource = dep_module.resource.as_ref().unwrap();
-            return Some(vec![
-              relative(&cwd, current_resource).to_string_lossy().into(),
-              relative(&cwd, dep_resource).to_string_lossy().into(),
-            ]);
-          }
-          // Found a cycle, but not for this module
-          continue;
-        }
+      // ignore dependencies that don't have an associated resource
+      if dep_module.resource.is_none() {
+        continue;
+      }
+      // ignore dependencies that are resolved asynchronously
+      if self.options.allow_async_cycles.unwrap_or(false) && dependency.weak {
+        continue;
+      }
+      // the dependency was resolved to the current module due to how webpack internals
+      // setup dependencies like CommonJsSelfReferenceDependency and ModuleDecoratorDependency
+      if current_module == dep_module {
+        continue;
+      }
 
-        if let Some(mut maybe_cyclical_paths_list) =
-          self.is_cyclic(initial_module, dep_module, seen_modules, compilation)
-        {
-          maybe_cyclical_paths_list.insert(
-            0,
-            relative(&cwd, &current_module.resource.unwrap())
-              .to_string_lossy()
-              .into(),
-          );
-          return Some(maybe_cyclical_paths_list);
+      if seen_modules.contains_key(&dep_module.debug_id) {
+        if dep_module.debug_id == initial_module.debug_id {
+          // Initial module has a circular dependency
+          let current_resource = current_module.as_context_module().unwrap();
+          let dep_resource = dep_module.resource.as_ref().unwrap();
+          return Some(vec![
+            relative(&cwd, current_resource).to_string_lossy().into(),
+            relative(&cwd, dep_resource).to_string_lossy().into(),
+          ]);
         }
+        // Found a cycle, but not for this module
+        continue;
+      }
+
+      if let Some(mut maybe_cyclical_paths_list) =
+        self.is_cyclic(initial_module, dep_module, seen_modules, compilation)
+      {
+        maybe_cyclical_paths_list.insert(
+          0,
+          relative(&cwd, &current_module.as_normal_module().unwrap())
+            .to_string_lossy()
+            .into(),
+        );
+        return Some(maybe_cyclical_paths_list);
       }
     }
 
@@ -117,16 +122,16 @@ impl CircularDependencyPlugin {
 }
 
 #[plugin_hook(CompilationOptimizeModules for CircularDependencyPlugin)]
-fn optimize_modules(&self, compilation: &mut Compilation) -> Result<Option<bool>> {
+async fn optimize_modules(&self, compilation: &mut Compilation) -> Result<Option<bool>> {
   let plugin = &self;
 
   // if plugin.options.on_start {
   //   plugin.options.on_start(compilation);
   // }
-  for module in compilation.modules.values() {
-    let should_skip = (module.resource.is_none()
+  for module in compilation.get_module_graph().modules() {
+    let should_skip = (module.resource_resolved_data().resource.to_string())
       || plugin.options.exclude.test(module.resource)
-      || !plugin.options.include.test(module.resource));
+      || !plugin.options.include.test(module.resource);
     // skip the module if it matches the exclude pattern
     if should_skip {
       continue;
@@ -135,23 +140,23 @@ fn optimize_modules(&self, compilation: &mut Compilation) -> Result<Option<bool>
     let maybe_cyclical_paths_list = &self.is_cyclic(module, module, {}, compilation);
     if let Some(cyclical_paths_list) = maybe_cyclical_paths_list {
       // allow consumers to override all behavior with onDetected
-      if let Some(on_detected) = &plugin.options.onDetected {
-        match on_detected(Module {
-          module,
-          paths: maybe_cyclical_paths_list,
-          compilation,
-        }) {
-          Ok(_) => {}
-          Err(err) => {
-            compilation.errors.push(err);
-          }
-        }
-        continue;
-      }
+      // if let Some(on_detected) = &plugin.options.on_detected {
+      //   match on_detected(&CircularDependencyDetectionContext {
+      //     module: &module,
+      //     paths: maybe_cyclical_paths_list,
+      //     compilation,
+      //   }) {
+      //     Ok(_) => {}
+      //     Err(err) => {
+      //       compilation.errors.push(err);
+      //     }
+      //   }
+      //   continue;
+      // }
 
       // mark warnings or errors on webpack compilation
       let error = Err(Error::new(maybe_cyclical_paths_list.join(" -> ")));
-      if plugin.options.failOnError {
+      if plugin.options.fail_on_error {
         compilation.errors.push(error);
       } else {
         compilation.warnings.push(error);
