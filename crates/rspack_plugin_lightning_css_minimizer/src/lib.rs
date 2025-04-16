e7711e209b9d35f9aa338bce3572aec6e973f396
Hana <andywangsy@gmail.com>
2025-04-16 18:21:26 +0800

    chore: lint

--- crates/rspack_plugin_lightning_css_minimizer/src/lib.rs
+++ crates/rspack_plugin_lightning_css_minimizer/src/lib.rs
@@ -268,7 +268,7 @@ async fn process_assets(&self, compilation: &mut Compilation) -> Result<()> {
                 .with_kind(DiagnosticKind::Css)
                 .with_severity(RspackSeverity::Warn).into()).boxed())
               } else {
-              Diagnostic::from(MinifyError(MietteDiagnostic::new(format!("LightningCSS minification warning: {}",e.to_string())).with_severity(rspack_error::miette::Severity::Warning).into()).boxed()).with_file(Some(filename.as_str().into()))
+              Diagnostic::from(MinifyError(MietteDiagnostic::new(format!("LightningCSS minification warning: {e}")).with_severity(rspack_error::miette::Severity::Warning).into()).boxed()).with_file(Some(filename.as_str().into()))
               }
             }),
           );
